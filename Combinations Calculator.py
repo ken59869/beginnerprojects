a20d47ec0dc7015017843828f610ce36b779036a
ken59869 <kenzahrai1@gmail.com>
2018-01-03 03:30:35 +0100

    int
    int

--- Combinations Calculator.py
+++ Combinations Calculator.py
@@ -1,6 +1,6 @@
 
-n = float(input())
-r = float(input())
+n = int(input())
+r = int(input())
 
 def factorial(number):
    if number < 1:   
@@ -8,7 +8,7 @@ def factorial(number):
    else:
        return number * factorial( number - 1 )
 nCr = factorial(n)/(factorial(r)*(factorial(n-r)))
-print(int(nCr))
+print(nCr)
 
 ##this is my combinations calculator. getting factorial recursively. really ghetto imo but does the job i guess
 
